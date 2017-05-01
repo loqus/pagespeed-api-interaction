9ad5958b32267076cae67b1a5f34ffe4646ade21
Loqus <git@loqus.nl>
2017-05-01 22:02:23 +0200

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,10 +1,14 @@
 <?php
-require 'vendor/autoload.php';
 require 'config.php';
+require 'vendor/autoload.php';
+
+$guzzleclient = new \GuzzleHttp\Client([
+    'timeout' => 60,
+    'verify' => false,
+]);
 
 //Build the API url and request
-//https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=https%3A%2F%2Fwww.mobiele-telefoons.nl
-&strategy=desktop&fields=formattedResults%2Cid%2CinvalidRules%2Ckind%2CpageStats%2CresponseCode%2CruleGroups%2Cscreenshot%2Ctitle%2Cversion&key={YOUR_API_KEY}
+//https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=https%3A%2F%2Fwww.mobiele-telefoons.nl&strategy=desktop&fields=formattedResults%2Cid%2CinvalidRules%2Ckind%2CpageStats%2CresponseCode%2CruleGroups%2Cscreenshot%2Ctitle%2Cversion&key={YOUR_API_KEY}
 
 $urltocheck = "https://www.yourdomain.com";
 
@@ -21,3 +25,9 @@ $psivariables[] = "key=".APIKEY;
 
 $psiurl .= urlencode(implode("&",$psivariables));
 
+//$res is in json format
+$res = $client->request('GET', $psiurl);
+
+$jsonobject = json_decode($res);
+
+print_r($jsonobject);
