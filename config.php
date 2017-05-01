d19d7e4dd7776a855e273842107c0eb317831b3f
Loqus <git@loqus.nl>
2017-05-01 22:17:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 
 //filter_third_party_resources exclude third party resources
-define("FILTER_THIRD_PARTY_RESOURCES",false);
+define("PSIFILTER_THIRD_PARTY_RESOURCES",false);
 
 //localize formatted results
-define("LOCALE","");
+define("PSILOCALE","nl_NL");
 
 //Add screenshot to results
-define("SCREENSHOT",true);
+define("PSISCREENSHOT",true);
 
 //strategy values mobile, desktop or both
-define("STRATEGY","desktop");
+define("PSISTRATEGY","desktop");
 
 //fields can include formattedResults,id,invalidRules,kind,pageStats,responseCode,ruleGroups,screenshot,title,version
-define("FIELDS","formattedResults,id,invalidRules,kind,pageStats,responseCode,ruleGroups,screenshot,title,version");
+define("PSIFIELDS","formattedResults,id,invalidRules,kind,pageStats,responseCode,ruleGroups,screenshot,title,version");
 
 //generate your own key here: https://console.developers.google.com/apis/credentials
-define("GOOGLEAPIKEY","");
+define("PSIAPIKEY","");
